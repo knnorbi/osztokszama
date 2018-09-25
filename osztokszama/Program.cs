f6a5dda6081a6e79258d6421a9c9ab95c0ee9093
knnorbi <kluka.norbert@gmail.com>
2018-09-25 14:48:35 +0200

    Initial commit

---
+++ osztokszama/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace osztokszama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Írj be egy számot!");
+            int szam = int.Parse(Console.ReadLine());
+            
+            int osztok = 0;
+            for (int i = 1; i <= szam; i++)
+                if (szam % i == 0)
+                    osztok++;
+
+            Console.WriteLine(osztok + "db osztója van!");
+
+            Console.ReadKey();
+        }
+    }
+}
